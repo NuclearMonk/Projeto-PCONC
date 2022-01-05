14129c259fdf8d6e60f254d1f5d3c160bc101bb5
Manuel <manuel.l.soares@tecnico.ulisboa.pt>
2022-01-05 00:40:51 +0000

    Comments and code cleanup

--- ap-paralelo-simples.c
+++ ap-paralelo-simples.c
@@ -1,12 +1,12 @@
 /******************************************************************************
  * Programacao Concorrente
- * MEEC 21/22
+ * MEEC 2021/2022
  *
- * Projecto - Parte1
+ * Projecto - Parte 1
  *                           ap-paralelo-simples.c
  * 
  * Compilacao: make ap-paralelo-simples
- *           
+ * Author: MAnuel Soares, Eduardo Faustino
  *****************************************************************************/
 #pragma region INCLUDES
 #include <stdlib.h>
@@ -20,17 +20,14 @@
 #pragma region ERROR CODES
 #define INVALID_ARGS 1
 #define DIR_NOT_FOUND 2
-
 #pragma endregion
 
-typedef struct ImageSet
-{
-    char **array;
-    unsigned int array_lenght;
-    unsigned int start_index;
-    unsigned int thread_count;
-} image_set;
-
+/**
+ * @brief Throws an error and exits the program
+ *
+ * 
+ * @param error_id Error codes defined in by macro at the top of this file
+ */
 void help(int error_id)
 {
     switch (error_id)
@@ -47,6 +44,14 @@ void help(int error_id)
     }
 }
 
+/**
+ * @brief verifica a extençao de um ficheiro dado o seu nome
+ *        Retorna falso se o ficheiro for composto apenas pela extenção, mesmo que a extensão seja a correta
+ * @param filename nome do ficheiro a verificar
+ * @param extention extenção contra a qual estamos a comparar
+ * @return true 
+ * @return false 
+ */
 bool check_file_ext(char *filename, char *extention)
 {
     const char *ext_dot = strrchr(filename, '.');
@@ -59,6 +64,13 @@ bool check_file_ext(char *filename, char *extention)
     return false;
 }
 
+/**
+ * @brief 
+ * Dado o string para um path retorna o numero de ficheiros png dentro desse path, retorna tambem um array com os nomes desses ficheiros
+ * @param path o path a explorar
+ * @param filenames um ponteiro onde guardar o array de string com o nome de todos os ficheiros .png
+ * @return int o numero de ficheiros png encontrado
+ */
 int list_pngs(char *path, char ***filenames)
 {
     unsigned int filecount = 0;
@@ -91,17 +103,27 @@ int list_pngs(char *path, char ***filenames)
     return filecount;
 }
 
-void *process_image_set(void *args)
+/**
+ * @brief struct de image set para passar todos os dados necessarios para cada thread poder processar um conjunto de imagens
+ * 
+ */
+typedef struct ImageSet
 {
-    image_set *set = (image_set *)args;
-    for (unsigned int i = set->start_index; i < set->array_lenght; i += set->thread_count)
-    {
-        printf("%u %s\n", set->start_index, set->array[i]);
-    }
-    free(args);
-    return NULL;
-}
+    char **array;
+    unsigned int array_lenght;
+    unsigned int start_index;
+    unsigned int thread_count;
+} image_set;
 
+/**
+ * @brief Create a image set object
+ * 
+ * @param array pointer to array of string with all the png file names
+ * @param array_lenght the lenght of the array of string
+ * @param start_index  the start index for the array
+ * @param thread_count the threeadcount being used
+ * @return image_set* 
+ */
 image_set *create_image_set(char **array, unsigned int array_lenght, unsigned int start_index, unsigned int thread_count)
 {
     image_set *thread_args = (image_set *)malloc(sizeof(image_set));
@@ -112,26 +134,50 @@ image_set *create_image_set(char **array, unsigned int array_lenght, unsigned in
     return thread_args;
 }
 
+/**
+ * @brief 
+ * Funcao de invocação para os threads de  processamento paralelo
+ * Cada thread executa as transformações necessárias sobre um subconjunto do array dados
+ * começando na posicao correspondent ao seu thread id e dando leapfrog saltando threadcount imagens de cada vez
+ * isto é feito assim para diminuir as discrepancias de trabalho entre threads tal que nenhum thread trabalhe em mais do que uma image que todos os outros
+ * @param args
+ * Um pointer void que aponta para um struct de tipo image_set
+ * @return void* 
+ */
+void *process_image_set(void *args)
+{
+    image_set *set = (image_set *)args;
+    for (unsigned int i = set->start_index; i < set->array_lenght; i += set->thread_count)
+    {
+        /*TODO Implement Image Transforming Function Calls*/
+        printf("%u %s\n", set->start_index, set->array[i]);
+    }
+    free(args);
+    return NULL;
+}
 
 int main(int argc, char *argv[])
 {
     unsigned int input_files_count = 0;
     char **input_files_names = NULL;
     int max_threads = 0;
+
     if (argc != 3)
         help(INVALID_ARGS);
+
     max_threads = atoi(argv[2]);
     input_files_count = list_pngs(argv[1], &input_files_names);
     pthread_t *threads = (pthread_t *)malloc(max_threads * sizeof(pthread_t));
+
     for (int i = 0; i < max_threads; i++)
     {
         pthread_create(&(threads[i]), NULL, process_image_set, create_image_set(input_files_names, input_files_count, i, max_threads));
     }
-
     for (int i = 0; i < max_threads; i++)
     {
         pthread_join(threads[i], NULL);
     }
+
     for (unsigned int i = 0; i < input_files_count; i++)
     {
         free(input_files_names[i]);
